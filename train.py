bf6c68bbd30a825a1145684f899af2980b3e74d1
TigDang <gizetdinov.m.f@gmail.com>
2023-05-07 15:19:09 +0300

    add training loop

--- train.py
+++ train.py
@@ -15,12 +15,13 @@ parser = argparse.ArgumentParser(
 
 parser.add_argument('--name', help='defines name of experiment')
 parser.add_argument('--epochs', type=int, help='how much training epochs need', default=10)
+parser.add_argument('-lr', '--learning_rate', type=float, help='defines learning rate', default=1e-4)
 parser.add_argument('-bs', '--batch_size', type=int, help='how much training examples will be used per training iteration', default=1)
 parser.add_argument('-m', '--modify', help='defines if modification will be used',
                     action='store_true')
 parser.add_argument('--contunue', help='defines if train will be started from last save',
                     action='store_true')
-parser.add_argument('--device', default='cuda:0', help='defines which c.u. will used; note, that multy-gpu not allowed')
+parser.add_argument('--device', default='cpu', help='defines which c.u. will used; note, that multy-gpu not allowed')
 
 if __name__=='__main__':
     opt = parser.parse_args()
@@ -31,15 +32,36 @@ if __name__=='__main__':
     if opt.modify:
         pass
     else:
-        model = LarssonVGG16()
+        model = LarssonVGG16().float().to(opt.device)
 
-    optimizer = torch.optim.Adam(model.parameters(), lr=1e-4)
+    optimizer = torch.optim.Adam(model.parameters(), lr=opt.learning_rate)
 
     # Define dataloader, which uses to load batches of images
     train_loader = DataLoader(train_dataset, batch_size=opt.batch_size, shuffle=True)
     val_loader = DataLoader(val_dataset, batch_size=opt.batch_size, shuffle=True)
 
     for epoch in range(opt.epochs):
+        print(f'\nepoch {epoch} had started')
+
+        # loop over the dataset multiple times
+        running_loss = 0.0
+        for i, data in enumerate(train_loader, 0):
+            l, ab = data
+            l, ab = l.float().to(opt.device), ab.float().to(opt.device)
+        
+            # zero the parameter gradients
+            optimizer.zero_grad()
+        
+            # forward + backward + optimize
+            outputs = model(l)
+            loss = loss_function(outputs, ab[:512]) + loss_function(outputs, ab[512:])
+            loss.backward()
+            optimizer.step()
+        
+            running_loss += loss.item()
+        
+        print(f'end of epoch, ')
+        print('Loss: {}'.format(running_loss))
         ## load batch
 
         ## generate colorization and trainc
